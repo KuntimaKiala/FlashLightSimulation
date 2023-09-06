fa490d2db907d01f4c2b20b95fe292b4ad5da1be
KuntimaKiala <Kuntima.kiala@gmail.com>
2023-09-06 15:51:46 +0200

    ignore make exec file

--- WindowManager.hpp
+++ WindowManager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <string_view>
 
 
 class WindowManager
@@ -8,5 +10,9 @@ class WindowManager
 public:
 	WindowManager();
 	~WindowManager() ;
+	void initialize(int width, int height,  const char* windowName) ; // a function that initializes glfw
+	
+private:
+
 };
 
