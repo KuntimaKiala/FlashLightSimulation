ff4977c189904cb90b6ea26e069998647390f15f
KuntimaKiala <Kuntima.kiala@gmail.com>
2023-09-06 15:50:59 +0200

    WIndow Creation and check if NULL

--- WindowManager.cpp
+++ WindowManager.cpp
@@ -8,3 +8,28 @@ WindowManager::WindowManager()
 WindowManager::~WindowManager()
 {
 }
+
+
+void WindowManager::initialize(int width, int height,  const char* windowName ) {
+
+
+
+    glfwInit() ;
+    glfwInitHint(GLFW_VERSION_MINOR, 3);
+    glfwInitHint(GLFW_VERSION_MAJOR, 3);
+    glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // window creation 
+    GLFWwindow *window =glfwCreateWindow( width, height, windowName, NULL, NULL );
+
+
+    if (window == NULL){
+
+        std::cout <<"WINDOWMANAGER::BUILD::FAILED\n";
+        std::cout <<"WINDOWMANAGER::RETURN::VOID\n" ;
+        return ;
+
+    }
+    glfwMakeContextCurrent(window) ;
+
+
+}
